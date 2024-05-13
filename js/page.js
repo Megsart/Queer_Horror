c300a5739c6d6895bcf6d36e9cf2ebec05cbee6e
megb13 <167036815+Megsart@users.noreply.github.com>
2024-05-12 22:45:47 -0700

    stuff?

---
+++ js/page.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+$("#title-page").append("<button id='start-button'>Start Game")
+
+$("#start-button").click(function(){
+    $("#title-page").toggleClass("");
+
+// toggle visibitiy of the start screen and show the other page I guess or link to another
+// check the gp page and see how we linked the library to the 
+
+});
